15b1ad580587f4793d743c0c5aa8b083863fe0ee
ganadara135 <ganadara135@gmail.com>
2019-11-26 13:32:34 +0900

    fix syntax error

--- src/RenderMediaFile.js
+++ src/RenderMediaFile.js
@@ -29,8 +29,8 @@ const RenderMultiMedia = ( {fileInfo}) => {
     },[fileInfo.id]);
 
     return (
-        <div>
-        { 
+    
+        
         checkImageFileType(fileInfo.fileType) ? <img src={fileInfo.src} width={"100%"} /> : 
         (checkVideoAudioFileType(fileInfo.fileType) ? <ReactPlayer width={"100%"} url={fileInfo.src} playing={false} controls={true} loop={true} /> : 
         (checkPDFFileType(fileInfo.fileType) ? 
@@ -52,8 +52,8 @@ const RenderMultiMedia = ( {fileInfo}) => {
     
             </div> : [] //<div><h1>{"This is not supporting type"}</h1></div>  
             ))
-        } 
-        </div>
+        
+        
     );
 };
 
