63fa0b58e9804aded3162c29194424fab6c2938a
ganadara135 <ganadara135@gmail.com>
2019-11-27 18:39:54 +0900

    test folder

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 //import FormContainer from "./FormContainer";
-module.exports = require('./FormContainer');
+//module.exports = require('./FormContainer');
 // module.exports = require('../src/RenderMediaFile');
-// console.log("테스트 단계 넘기기");
+console.log("테스트 단계 넘기기");
